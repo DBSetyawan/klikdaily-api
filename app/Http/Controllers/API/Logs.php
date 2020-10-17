5e91f16b17756a272e3eca123ebe9a929cbe1883
Daniel <artexsdns@gmail.com>
2020-10-17 16:01:55 +0700

    added logs not found

--- app/Http/Controllers/API/Logs.php
+++ app/Http/Controllers/API/Logs.php
@@ -7,30 +7,42 @@ use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use App\Http\Resources\LogResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Responsefail;
 
 class Logs extends Controller
 {
     public function getByLogs($location_id){
         $d = Log::whereIn('location_id', [$location_id])->get();
 
-        foreach ($d as $key => $value) {
-            # code...
-            $location_id = $value->location_id;
-            $location_name = $value->location_name;
-            $product = $value->product;
-            $current_qty = $value->location->quantity;
-            $dsa[] = $value;
+        // dd($d);
 
-        }
-
-        foreach ($dsa as $key => $value) {
-
-            $logs[$key] = Arr::only($value->toArray(),['id','created_at','type','adjustment','quantity']);
-            unset($value->location);
+        if($d->isEmpty()){
             
-        }
+            return Responsefail::fails(400, "Failed, logs not found");
 
-            return LogResponse::success(200, $logs, "Success, log found", $location_id, $location_name, $product, $current_qty);
+        } else {
+
+
+            foreach ($d as $key => $value) {
+                # code...
+                $location_id = $value->location_id;
+                $location_name = $value->location_name;
+                $product = $value->product;
+                $current_qty = $value->location->quantity;
+                $dsa[] = $value;
+    
+            }
+    
+            foreach ($dsa as $key => $value) {
+    
+                $logs[$key] = Arr::only($value->toArray(),['id','created_at','type','adjustment','quantity']);
+                unset($value->location);
+                
+            }
+    
+                return LogResponse::success(200, $logs, "Success, log found", $location_id, $location_name, $product, $current_qty);
+
+        }
 
     }
 }
---
+++ app/Http/Resources/Responsefail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+class Responsefail
+{
+   protected static $response = [
+
+            'status_code' => null,
+            'error_message' => null
+   ];
+
+   public static function fails($code = null, $message = null){
+        
+          self::$response['status_code'] = $code;
+          self::$response['error_message'] = $message;
+
+        return response()->json(self::$response);
+
+   }
+
+}
